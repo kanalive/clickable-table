a5cb79a58f54d67a4e12accfad580b732b21eced
Karina <wenli@live.com.au>
2024-04-08 21:18:45 +1000

    updated readme and example

--- build/lib/clickable_table/example.py
+++ build/lib/clickable_table/example.py
@@ -1,28 +1,25 @@
 import streamlit as st
 from clickable_table import clickable_table
 import pandas as pd
-# Add some test code to play with the component while it's in development.
-# During development, we can run this just as we would any other Streamlit
-# app: `$ streamlit run my_component/example.py`
 
 st.subheader("Test Clickable Table")
 
 data = {
-'Epoch': ["1 Fut", "2 Fut", "3 Fut", "4 Fut", "5 Fut", "6 Fut", "7 Fut", "8 Fut"],
-'2 Fut': [0.081, -1.164, -1.635, -1.515, -1.460, -1.306, -1.136, -1.222],
-'3 Fut': [-0.505, 1.461, 1.596, 1.510, 1.406, 1.241, 1.198, 0.932],
-'4 Fut': [0.987, -1.527, -1.583, -1.475, -1.348, -1.261, -1.037, -0.851],
-'5 Fut': [-1.226, 1.553, 1.552, 1.429, 1.352, 1.150, 0.966, 0.782],
-'6 Fut': [1.290, 1.382, 1.361, 1.268, 1.160, 1.028, 0.899, None]  # Assuming the last entry is missing or NaN
+'Epoch': ["R 1", "R 2", "R 3", "R 4", "R 5", "R 6", "R 7", "R 8"],
+'C 1': [0.081, -1.164, -1.635, -1.515, -1.460, -1.306, -1.136, -1.222],
+'C 2': [-0.505, 1.461, 1.596, 1.510, 1.406, 1.241, 1.198, 0.932],
+'C 3': [0.987, -1.527, -1.583, -1.475, -1.348, -1.261, -1.037, -0.851],
+'C 4': [-1.226, 1.553, 1.552, 1.429, 1.352, 1.150, 0.966, 0.782],
+'C 5': [1.290, 1.382, 1.361, 1.268, 1.160, 1.028, 0.899, None]  
 }
 
 def style_dataframe(df, header_color, index_color):
     # Function to apply conditional formatting
     def apply_conditional_formatting(val):
-        if val > 0:
-            color = "#FFC0CB"  # Light pink for positive values in '3 Fut'
-        elif val < 0:
-            color = "#90EE90"  # Light green for negative values in '5 Fut'
+        if val > 1.5:
+            color = "#FFC0CB"  
+        elif val < -1.5:
+            color = "#90EE90"  
         else:
             color = ''
         return f'background-color: {color}'
@@ -36,13 +33,7 @@ def style_dataframe(df, header_color, index_color):
     # Initialize Styler object with table styles
     styler = df.style.set_table_styles(styles)
 
-    # Apply conditional formatting to the '3 Fut' column for positive values
-    if '3 Fut' in df.columns:
-        styler = styler.applymap(apply_conditional_formatting, subset=['3 Fut'])
-
-    # Apply conditional formatting to the '5 Fut' column for negative values
-    if '5 Fut' in df.columns:
-        styler = styler.applymap(apply_conditional_formatting, subset=['5 Fut'])
+    styler = styler.applymap(apply_conditional_formatting)
 
     return styler
 
